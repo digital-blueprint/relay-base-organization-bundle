8dadf912f7f8d07ed22b06c3a1d0fc42fd2d832e
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-07-10 12:41:29 +0200

    adaptions to core bundle update

--- src/DataProvider/OrganizationDataProvider.php
+++ src/DataProvider/OrganizationDataProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Dbp\Relay\BaseOrganizationBundle\DataProvider;
 
 use Dbp\Relay\BaseOrganizationBundle\API\OrganizationProviderInterface;
+use Dbp\Relay\BaseOrganizationBundle\Entity\Organization;
 use Dbp\Relay\CoreBundle\Rest\AbstractDataProvider;
 
 class OrganizationDataProvider extends AbstractDataProvider
@@ -24,15 +25,15 @@ class OrganizationDataProvider extends AbstractDataProvider
         return $this->isAuthenticated();
     }
 
-    protected function getItemById(string $id, array $filters = [], array $options = []): object
+    protected function getItemById(string $id, array $filters = [], array $options = []): ?object
     {
         return $this->organizationProvider->getOrganizationById($id, $options);
     }
 
     protected function getPage(int $currentPageNumber, int $maxNumItemsPerPage, array $filters = [], array $options = []): array
     {
-        if ($search = ($filters['search'] ?? null)) {
-            $options['search'] = $search;
+        if ($search = ($filters[Organization::SEARCH_PARAMETER_NAME] ?? null)) {
+            $options[Organization::SEARCH_PARAMETER_NAME] = $search;
         }
 
         return $this->organizationProvider->getOrganizations($currentPageNumber, $maxNumItemsPerPage, $options);
