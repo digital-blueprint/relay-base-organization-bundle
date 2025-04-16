5dd6623eafe95a5d30038bbf64d8dbc35a5ea23c
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-16 12:53:31 +0200

    Port api-platform metadata from yaml to PHP attributes

--- src/Entity/Organization.php
+++ src/Entity/Organization.php
@@ -4,20 +4,78 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BaseOrganizationBundle\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Parameter;
+use Dbp\Relay\BaseOrganizationBundle\DataProvider\OrganizationDataProvider;
 use Dbp\Relay\CoreBundle\LocalData\LocalDataAwareInterface;
 use Dbp\Relay\CoreBundle\LocalData\LocalDataAwareTrait;
 use Dbp\Relay\CoreBundle\Rest\Entity\NamedEntityInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    shortName: 'BaseOrganization',
+    types: ['http://schema.org/Organization'],
+    operations: [
+        new Get(
+            uriTemplate: '/base/organizations/{identifier}',
+            openapi: new Operation(
+                tags: ['BaseOrganization'],
+                parameters: [
+                    new Parameter(
+                        name: 'includeLocal',
+                        in: 'query',
+                        description: 'Local data attributes to include',
+                        schema: ['type' => 'string'],
+                        example: 'code',
+                    ),
+                ]
+            ),
+            provider: OrganizationDataProvider::class
+        ),
+        new GetCollection(
+            uriTemplate: '/base/organizations',
+            openapi: new Operation(
+                tags: ['BaseOrganization'],
+                parameters: [
+                    new Parameter(
+                        name: 'search',
+                        in: 'query',
+                        description: "Search filter (partial, case-insensitive text search on 'name' attribute)",
+                        required: false,
+                        schema: ['type' => 'string'],
+                    ),
+                    new Parameter(
+                        name: 'includeLocal',
+                        in: 'query',
+                        description: 'Local data attributes to include',
+                        schema: ['type' => 'string'],
+                        example: 'code',
+                    ),
+                ]
+            ),
+            provider: OrganizationDataProvider::class
+        ),
+    ],
+    normalizationContext: [
+        'groups' => ['BaseOrganization:output', 'LocalData:output'],
+        'jsonld_embed_context' => true,
+    ]
+)]
 class Organization implements LocalDataAwareInterface, NamedEntityInterface
 {
     use LocalDataAwareTrait;
 
     public const SEARCH_PARAMETER_NAME = 'search';
 
+    #[ApiProperty(identifier: true)]
     #[Groups(['BaseOrganization:output'])]
     private ?string $identifier = null;
 
+    #[ApiProperty(iris: ['https://schema.org/name'])]
     #[Groups(['BaseOrganization:output'])]
     private ?string $name = null;
 
